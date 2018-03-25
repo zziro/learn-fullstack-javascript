685cf5f17463b4dad948874775950cb03bf0bbcc
zziro <zziro.123@gmail.com>
2018-03-25 18:41:28 -0500

    Refactoring the code before to work with routing

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,19 @@ const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
 
 class App extends React.Component {
-  state = {
-    pageHeader: 'Naming Contests',
-    contests: this.props.initialContests
+  static propTypes = {
+    initialData: React.PropTypes.object.isRequired
   };
+
+  state = this.props.initialData;
   componentDidMount() {
 
   }
+
   componentWillUnmount() {
     // clean timers, listeners
   }
+
   fetchContest = (contestId) => {
     pushState(
       { currentContestId: contestId },
@@ -25,7 +28,6 @@ class App extends React.Component {
     );
     api.fetchContest(contestId).then(contest => {
       this.setState({
-        pageHeader: contest.contestName,
         currentContestId: contest.id,
         contests: {
           ...this.state.cotests,
@@ -34,19 +36,31 @@ class App extends React.Component {
       });
     });
   };
+
+  currentContest(){
+      return this.state.contests[this.state.currentContestId];
+  }
+
+  pageHeader(){
+    if(this.state.currentContestId){
+      return this.currentContest().contestName;
+    }
+    return 'Naming Contests';
+  }
+
   currentContent() {
     if (this.state.currentContestId) {
-      return <Contest {...this.state.contests[this.state.currentContestId]} />;
-    }
-
+      return <Contest {...this.currentContest()} />;
+  }
     return <ContestList
             onContestClick={this.fetchContest}
             contests={this.state.contests} />;
   }
-  render() {
+
+   render() {
     return (
       <div className="App">
-        <Header message={this.state.pageHeader} />
+        <Header message={this.pageHeader()} />
         {this.currentContent()}
       </div>
     );
