ed0d9b72added4b3e6995ded85c60b1a244bc261
zziro <zziro.123@gmail.com>
2018-03-25 14:40:38 -0500

    Handling the contest click envent

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+
+import ContestList  from './ContestList';
 
 class App extends React.Component {
   state = {
@@ -17,11 +18,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
-        <div>
-          {this.state.contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest} />
-          )}
-        </div>
+        <ContestList contests={this.state.contests} />
       </div>
     );
   }
