b5c2377abc277aa2bf2c31379b8df38b8e847815
zziro <zziro.123@gmail.com>
2018-03-24 23:31:31 -0500

    Preloading data

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import config from './config';
-import express from 'express';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
 
+import express from 'express';
 const server = express();
 
 server.use(sassMiddleware({
@@ -13,15 +13,17 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 
+import './serverRender';
+
 server.get('/', (req, res) => {
   res.render('index', {
-    content: 'Hello Express and <em>EJS</em>'
+    content: '...'
   });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
 });
